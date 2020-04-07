4fb1343468bf4f2f2df4aaa656ee81271e1c0d51
Willian Amaral <willian.amaral@outlook.com.br>
2020-04-07 18:28:07 -0300

    Import Home from pages

--- template/src/App.js
+++ template/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Home from 'pages/Home';
+
 const App = () => {
   return (
     <Router>
